6d1583f4f6b0df5a48e0cc7521d8890a56e53736
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2024-09-02 02:40:24 +0500

    data folder deleted

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function updatePageTitle(category) {
 }
 
 function loadQuestions(category) {
-    const filePath = `/data/${category}_questions.json`;
+    const filePath = `${category}_questions.json`;
     
     fetch(filePath)
         .then(response => response.json())
