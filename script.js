52b7b784a609553651e11e379143052010e58510
Ashique Ali <60208343+ashiqaliaslam@users.noreply.github.com>
2024-09-02 10:15:40 +0500

    english btn added with href

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ function updatePageTitle(category) {
 }
 
 function loadQuestions(category) {
-    const filePath = `${category}_questions.json`;
+    const filePath = `https://ashiqaliaslam.github.io/test-preparation/${category}_questions.json`;
     
     fetch(filePath)
         .then(response => response.json())
